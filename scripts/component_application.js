81ddc090709f8025492e0c723015e62b042653fb
pizzafaceopossum <pizzafaceopossum@gmail.com>
2024-10-31 16:23:12 -0400

    Fixed child components not being processed correctly (order of operations was wrong)

--- scripts/component_application.js
+++ scripts/component_application.js
@@ -1,3 +1,12 @@
+/*
+ *	Component Application
+ *	Order of operations:
+ *	1. Place Child Elements
+ *	2. Apply Attribute Inheritance
+ *	4. Apply Style Classes
+ *	3. Apply Child Components
+ */
+
 	// Applies attribute inheritance to inheritors
 	// If the inheritor defines that attribute, it will not be overwritten
 	// Style is unique, it has style attributes which are themselves separately inherited
@@ -7,7 +16,7 @@ function applyAttributeInheritance(elem, attributes, styleAttributes)
 	const attributesToInherit = [...elem.attributes.all ? attributes : elem.attributes].filter(attr => !ignoredAttributes.includes(attr.name.toLowerCase()));
 	const styleToInherit = (elem.attributes.all && elem.attributes.style == null) || (elem.attributes.style && /all/.test(elem.attributes.style.value)) ? Object.keys(styleAttributes) : elem.attributes.style.value.split(';').map(e=>e.trim()).filter(e=>Object.keys(styleAttributes).includes(e)&&e.length > 0);
 
-	console.log('elem', elem, 'given attributes', attributes, 'given style', styleAttributes, 'toInherit', attributesToInherit, 'styleToInherit', styleToInherit);
+	//console.log('elem', elem, 'given attributes', attributes, 'given style', styleAttributes, 'toInherit', attributesToInherit, 'styleToInherit', styleToInherit);
 	if (elem.childElementCount > 0)
 	{
 		for (const child of elem.children)
@@ -39,7 +48,7 @@ function applyAttributeInheritance(elem, attributes, styleAttributes)
 			}
 		}
 	}
-	elem.outerHTML = elem.getHTML();
+	elem.outerHTML = elem.getHTML().trim();
 }
 
 function applyStyleClasses(elements)
@@ -72,7 +81,6 @@ function applyStyleClasses(elements)
 		element.setAttribute('style', styleStringFromObject(styleAttributes));
 		element.removeAttribute('style-class');
 	}
-	
 }
 
 function applyComponent(component)
@@ -90,38 +98,45 @@ function applyComponent(component)
 	}
 
 		// Get a duplicate of the component to edit into the instance
-	const loadedComponent = copyLoadedComponent(component.attributes.ctype.nodeValue);
-	console.log(loadedComponent);
-	applyComponents(loadedComponent);
-	let inheritor = loadedComponent.querySelector('cInherit');
-		// Locate any attribute inheritors in the component
-	while (inheritor != null)
-	{
-		console.log('inheritor', inheritor);
-		applyAttributeInheritance(inheritor, component.attributes, styleAttributes);
-		inheritor = loadedComponent.querySelector('cInherit');
-	}
-	applyStyleClasses(loadedComponent.querySelectorAll('[style-class]'));
-
-		// Look for '<children></children>' in the component file
-	const childPlacement = loadedComponent.querySelector('cChildren');
-
+	const componentDef = copyLoadedComponent(component.attributes.ctype.nodeValue);
+		// Look for '<cchildren></cchildren>' in the component file
+	const childPlacement = componentDef.querySelector('cChildren');
 		// If the component HTML says where to place the child HTML, place it there
 	if (childPlacement)
 	{
 		childPlacement.outerHTML = innerHTML;
-		component.outerHTML = loadedComponent.getHTML().trim();
 	}
 	else	// Place it at the end, otherwise
 	{
-		component.outerHTML = loadedComponent.getHTML().trim() + innerHTML;
+
+		componentDef.innerHTML += innerHTML;
 	}
+	
+	for (const child of componentDef.children)
+	{
+		// After placing the children, we need to apply components to them.
+		applyComponents(child);
+	}
+	
+	let inheritor = componentDef.querySelector('cInherit');
+		// Locate any attribute inheritors in the component
+	while (inheritor != null)
+	{
+		applyAttributeInheritance(inheritor, component.attributes, styleAttributes);
+		inheritor = componentDef.querySelector('cInherit');
+	}
+	applyStyleClasses(componentDef.querySelectorAll('[style-class]'));
+	component.outerHTML = componentDef.getHTML().trim();
 }
 
 	// Replace any components that are children of 'element' with the HTML that they stand for.
 function applyComponents(element)
 {
-	if (element.childElementCount > 0)
+	if (element.tagName.toLowerCase() == 'component')
+	{
+		applyComponent(element);
+	}
+	else
 	{
 			// If there are any element children, recursively call this function, so that components are replaced 'depth-first'.
 			// This must be called for all element types, otherwise '<component><div><component>...</component></div></component>' would not work.
@@ -129,15 +144,5 @@ function applyComponents(element)
 		{
 			applyComponents(child);
 		}
-	}
-		// If this element is a component, then execute the replacement function.
-	switch (element.tagName.toLowerCase())
-	{
-		case 'component':
-			applyComponent(element);
-			break;
-		//case 'cinherit':
-		//	applyAttributeInheritance(element);
-		//	break;
 	}
 }
