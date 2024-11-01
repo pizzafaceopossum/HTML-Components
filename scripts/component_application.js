e47277c005e162a8d1e5785fe96d91e044cbe1fc
pizzafaceopossum <pizzafaceopossum@gmail.com>
2024-11-01 12:35:11 -0400

    Fixed some small problems introduced in the last commit w.r.t. style inheritance

--- scripts/component_application.js
+++ scripts/component_application.js
@@ -18,52 +18,52 @@ function applyAttributeInheritance(elem, attributes, styleAttributes)
 	console.log('component attributes', attributes);
 	console.log('component style attributes', styleAttributes);
 	*/
-		// Don't even bother to do anything if the inheritor doesn't say to inherit any attributes or the component has none set
-	if ([...elem.attributes].length > 0 && [...attributes].filter(attr => !ignoredAttributes.includes(attr.name.toLowerCase())).length + Object.keys(styleAttributes).length > 0)
+		// Don't even bother to do anything if the inheritor doesn't say to inherit any attributes or the component has none set or the inheritor has no contained elements
+	if (elem.childElementCount > 0 && [...elem.attributes].length > 0 && [...attributes].filter(attr => !ignoredAttributes.includes(attr.name.toLowerCase())).length + Object.keys(styleAttributes).length > 0)
 	{
 			// Inherit attributes: If the 'all' attribute is set, just do them all, otherwise only do the ones that are set
 		let attributesToInherit = [...elem.attributes.all ? attributes : elem.attributes].filter(attr => !ignoredAttributes.includes(attr.name.toLowerCase()));
-			// Inherit style attributes: If 
+			// Inherit style attributes: Start by taking the defined style attributes
 		let styleToInherit = Object.keys(styleAttributes);
 
+			// Then filter by which the inheritor says to inherit, if any
 		if (elem.attributes.style)
 		{
 			styleToInherit = /all/.test(elem.attributes.style.value) ? styleToInherit : styleToInherit.filter(e => RegExp(e).test(elem.attributes.style.value));
 		}
 		else
 		{
-			styleToInherit = [];
+			styleToInherit = elem.attributes.all ? styleToInherit : [];
 		}
 		//console.log('elem', elem, 'given attributes', attributes, 'given style', styleAttributes, 'toInherit', attributesToInherit, 'styleToInherit', styleToInherit);
-		if (elem.childElementCount > 0)
+		for (const child of elem.children)
 		{
-			for (const child of elem.children)
+				// Attributes overwrite those already set, when they exist on the parent instance
+			for (attribute of attributesToInherit)
 			{
-					// Attributes overwrite those already set, when they exist on the parent instance
-				for (attribute of attributesToInherit)
+				child.attributes.setNamedItem(attribute.cloneNode(true));
+			}
+			
+			if (styleToInherit.length > 0)
+			{
+					// Get child's current style
+				const childStyleAttributes = {};
+				if (child.attributes.style)
 				{
-					child.attributes.setNamedItem(attribute.cloneNode(true));
-				}
-
-				if (styleToInherit.length > 0)
-				{
-					const childStyleAttributes = {};
-					if (child.attributes.style)
+					for (const attr of getStyleFromString(child.attributes.style.value))
 					{
-						for (const attr of getStyleFromString(child.attributes.style.value))
-						{
-							childStyleAttributes[attr.name] = attr.value;
-						}
+						childStyleAttributes[attr.name] = attr.value;
 					}
-					Object.assign(childStyleAttributes, styleAttributes);
-					child.setAttribute('style', styleStringFromObject(childStyleAttributes));
 				}
+					// Overwrite/add the inherited style
+				for (const attr of styleToInherit)
+				{
+					childStyleAttributes[attr] = styleAttributes[attr];
+				}
+					// Set the child's style attribute
+				child.setAttribute('style', styleStringFromObject(childStyleAttributes));
 			}
 		}
-	}
-	else
-	{
-		console.log('def');
 	}
 	elem.outerHTML = elem.getHTML().trim();
 }
@@ -125,24 +125,29 @@ function applyComponent(component)
 	}
 	else	// Place it at the end, otherwise
 	{
-
 		componentDef.innerHTML += innerHTML;
 	}
 	
-	for (const child of componentDef.children)
-	{
-		// After placing the children, we need to apply components to them.
-		applyComponents(child);
-	}
-	
-	let inheritor = componentDef.querySelector('cInherit');
 		// Locate any attribute inheritors in the component
+	let inheritor = componentDef.querySelector('cInherit');
+	
+		// While there are inheritors, apply the inheritance to them
 	while (inheritor != null)
 	{
 		applyAttributeInheritance(inheritor, component.attributes, styleAttributes);
 		inheritor = componentDef.querySelector('cInherit');
 	}
+	
+		// Apply style classes to any elements that have them
 	applyStyleClasses(componentDef.querySelectorAll('[style-class]'));
+	
+		// Then, recursively apply components to child components
+	for (const child of componentDef.children)
+	{
+		applyComponents(child);
+	}
+	
+		// Finally, remove the component's tag from the document
 	component.outerHTML = componentDef.getHTML().trim();
 }
 
